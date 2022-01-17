75a79e681ad9abad487204de64cce6c431b832db
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2022-01-17 15:26:59 -0300

    Update json_to_evfl.py

--- json_to_evfl.py
+++ json_to_evfl.py
@@ -2,17 +2,7 @@
 from evfl import EventFlow, Flowchart, Event, ActionEvent, SwitchEvent, ForkEvent, JoinEvent, SubFlowEvent, Actor, Container
 from evfl.common import ActorIdentifier, StringHolder
 from evfl.util import IdGenerator, RequiredIndex
-from evfl.repr_util import generate_flowchart_graph
 import json
-
-# flow = EventFlow()
-# with open("Demo007_2.bfevfl", "rb") as f:
-#     flow.read(f.read())
-
-# json_obj = json.dumps(generate_flowchart_graph(flow), indent=4, default=lambda x: str(x))
-
-# with open("event.json", "w") as out:
-#     out.write(json_obj)
 
 with open("event.json", "r") as in_file:
     json_read = json.load(in_file)
@@ -22,72 +12,55 @@ flow.name = "Name"
 flow.flowchart = Flowchart()
 flow.flowchart.name = "Flow"
 flowchart = flow.flowchart
-# flowchart = Flowchart()
 
 json_read = json_read
 
-
 i = 0
 while i < len(json_read):
+    # Initialize nodes and edges
     node = json_read[i]
     edge = json_read[i+1]
+
+    # Initialize events, actors and params
     event = Event()
+
+    actor = Actor()
+    actor.identifier.name = node['data']['actor'] if node['data']['actor'] else None
+    actor.actions.append(StringHolder(node['data']['action']) if node['data']['action'] else None)
+    actor.queries.append(StringHolder(node['data']['query']) if node['data']['query'] else None)
+
+    params = Container()
+    params.data = i['data']['params'] if i['data']['params'] else None
+
+    flowchart.add_event(node, IdGenerator())
+    event.name = node['data']['name']
     if node['node_type'] == 'action':
         event.data = ActionEvent()
+        event.data.actor = actor
+        flowchart.actors.v.append(actor)
+        event.data.actor_action.v = actor.actions[0]
+        event.data.params = params
 
+    elif node['node_type'] == 'switch':
+        event.data = SwitchEvent()
+        event.data.actor = actor
+        flowchart.actors.v.append(actor)
+        event.data.params = params
+        # TODO: connect edges
 
-# for i in json_read:
-#     event = Event()
-#     if i['type'] == 'node':
-#         if i['node_type'] == 'action':
-#             event.data = ActionEvent()
-#             actor = Actor()
-#             actor.identifier.name = i['data']['actor']
-#             actor.actions.append(StringHolder(i['data']['action']))
+    elif node['node_type'] == 'fork':
+        continue
 
-#             flowchart.actors.append(actor)
+    elif node['node_type'] == 'join':
+        continue
 
-#             event.data.actor.v = actor
+    elif node['node_type'] == 'sub_flow':
+        event.data = SubFlowEvent()
+        event.data.res_flowchart_name = node['data']['res_flowchart_name']
+        event.data.entry_point_name = node['data']['entry_point_name']
+        event.data.params = params
 
-#             event.data.actor_action.v = actor.actions[0]
-
-#             event.data.params = Container()
-#             event.data.params.data = i['data']['params'] if i['data']['params'] else None
-#         elif i['node_type'] == 'switch':
-#             event.data = SwitchEvent()
-#             actor = Actor()
-#             actor.identifier.name = i['data']['actor']
-#             actor.queries.append(StringHolder(i['data']['query']))
-#             flowchart.actors.append(actor)
-#             event.data.actor.v = actor
-#             event.data.actor_query.v = actor.queries[0]
-#             event.data.params = Container()
-#             event.data.params.data = i['data']['params'] if i['data']['params'] else None
-#             edge = next(json_read)
-#             target = RequiredIndex(edge['target'])
-#             event.data.cases[i['id']] = target
-#             event.data.cases[i['id']].v = event # Needs to be the next event. Just sets the current event for now
-#         elif i['node_type'] == 'fork':
-#             continue
-#             # event.data = ForkEvent()
-#         elif i['node_type'] == 'join':
-#             continue
-#             # event.data = JoinEvent()
-#         elif i['node_type'] == 'sub_flow':
-#             event.data = SubFlowEvent()
-#             event.data.res_flowchart_name = i['data']['res_flowchart_name']
-#             event.data.entry_point_name = i['data']['entry_point_name']
-#             event.data.params = Container()
-#             event.data.params.data = i['data']['params'] if i['data']['params'] else None
-#         elif i['node_type'] == 'entry':
-#             continue
-#         flowchart.add_event(event, IdGenerator())
-#         event.name = i['data']['name']
-#     elif i['type'] == 'edge':
-#         pass
+    i += 2
     
-
-# new_flow.flowchart = flowchart
-# new_flow.flowchart.name = "name"
 with open("test.bfevfl", "wb") as f_mod:
     flow.write(f_mod)
