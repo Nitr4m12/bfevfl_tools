19b7cb4a07883b94e0f6ea44f46a0c3062a4409c
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2022-01-17 19:35:50 -0300

    Updated evfl_to_json.py

--- evfl_to_json.py
+++ evfl_to_json.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 from evfl import EventFlow
 from evfl.repr_util import generate_flowchart_graph
+from pathlib import Path
 import json
+import sys
 
+
+file = Path(sys.argv[1])
 flow = EventFlow()
-with open("Demo007_2.bfevfl", "rb") as f:
+with open(file, "rb") as f:
     flow.read(f.read())
 
 json_obj = json.dumps(generate_flowchart_graph(flow), indent=4, default=lambda x: str(x))
 
-with open("Demo007_2.json", "w") as out:
+with open(file.with_suffix(".json"), "w") as out:
     out.write(json_obj)
